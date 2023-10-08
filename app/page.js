b265aec4923c4431d2d55b5b94e5413bf2c2307d
yeeyee6847 <hoe1686@gmail.com>
2023-10-09 01:50:37 +0800

    remove unuse file

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
-'use client'
-import SignIn from "@/hooks/useSession"
-import { SessionProvider } from "next-auth/react"
-
 export default function App() {
   return (
-    <SignIn></SignIn>
+    <>
+    </>    
   )
 }
